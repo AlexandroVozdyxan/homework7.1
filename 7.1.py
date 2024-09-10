0a8d453c4ebdbf16ced3c4b0552ad3f8d752127e
vovav <vovavitka10@gmail.com>
2024-09-10 17:37:13 +0300

    7.1

---
+++ 7.1.py
@@ -0,0 +1,5 @@
+def say_hi(name, age):
+    print(f"Hi. My name is {name} and I`m {age} years old")
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+say_hi(user_name, user_age)
